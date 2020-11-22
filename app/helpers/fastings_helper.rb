396667cdc52117d86eb4091bd8551780aa764ebb
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-22 18:24:06 +0900

    first commit

---
+++ app/helpers/fastings_helper.rb
@@ -0,0 +1,2 @@
+module FastingsHelper
+end
---
+++ test/controllers/fastings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FastingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
