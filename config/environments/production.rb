a3aed50653395a075104996999acfccde901f57d
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-29 19:54:16 +0900

    本番環境で画像を読み込めるように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.public_file_server.enabled = true
+
   #テンプレート使う場合は記述する
   host = 'samplehost'
   Rails.application.routes.default_url_options[:host] = host
