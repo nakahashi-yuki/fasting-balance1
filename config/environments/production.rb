b830324f9652a0ec06ee02e6221962ac151150a3
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-29 19:59:58 +0900

    本番環境で画像を読み込めるように修正2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -110,7 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.public_file_server.enabled = true
+  
 
   #テンプレート使う場合は記述する
   host = 'samplehost'
